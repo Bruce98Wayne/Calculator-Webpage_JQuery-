a707d1b4f6e588437667bbfb7d6258ae3b79a070
Akshat Goel <akshatgoel998@gmail.com>
2018-07-07 18:29:26 +0530

    All Functions are working. Thorough testing is pending though!

--- calc.js
+++ calc.js
@@ -1,11 +1,14 @@
 var display = $('input[name = "display"]');
 var value = display.val();
-//console.log(value)
-
+var status1 = 0;
+var prevOperator;
 var prevVal;
 var operator;
 var check = 0;
 var numArr = [];
+var prevA;
+var noSecondArg = 0;
+var multiEqual =0;
 for(var i=0; i<10; i++){
     numArr.push($(`#button${i}`))
 };
@@ -18,84 +21,142 @@ arithArr.push(  $('#addButton'),
                 $('#divideButton')
             );
 
-///console.log(arithArr[0].text());
+
 var equal = $('#equalsButton');
 var clear = $('#clearButton')
 
 
-//display.click();
 if(check ===0){
 for(var i=0; i<10; i++){
     numArr[i].click(function(){
 
         if((!/\d/.test(display.val()))  && check===0){
             display.val(parseInt($(this).val()));
-            console.log(check);
         }
 
 
         else if((/\d/.test(display.val()))  && check===0) {
             display.val(parseInt(display.val() + $(this).val()));
-            console.log(check);
         }
+
+        else if(check===1 ){
+            prevVal = display.val();
+            display.val(parseInt($(this).val()));
+            check =2;
+        }
+
+        else if(/\d/.test(display.val()) && check ===2){
+            display.val(parseInt(display.val() + $(this).val()));
+        }
+
+        prevA = display.val();
+        console.log('PrevA: ' + prevA);
+        noSecondArg = 0;
     });
 }
 }
+
+
 
 for(var i=0; i<arithArr.length; i++){
 
     arithArr[i].click(function(){
 
-        operator = $(this).val();
-        //console.log(operator);
-        $('#output').html(display.val());
-        check = 1;
-        //console.log(check);
+        if(status1 === 1){
 
-            for(var i=0; i<10; i++){
-                numArr[i].click(function(){
+            if(prevOperator === '+'){
+                prevVal = parseFloat(prevVal) + parseFloat(display.val());
+            }
+            else if(prevOperator === '-')
+                prevVal =parseFloat(prevVal) - parseFloat(display.val());
 
-                if(check===1 ){
-                    prevVal = display.val();
-                    display.val(parseInt($(this).val()));
-                    console.log(check);
-                    check =2;
-                }
+            else if(prevOperator === '*')
+                prevVal = parseFloat(prevVal)*parseFloat(display.val());
 
-                else if(/\d/.test(display.val()) && check ===2){
-                    display.val(parseInt(display.val() + $(this).val()));
-                    console.log(check);
-                }
-            });
+            else if(prevOperator === '/'){
+                if(display.val() === 0)
+                    display.val('infinity');
+                else
+                    prevVal = parseFloat(prevVal)/parseFloat(display.val());
             }
 
-        //display.val($(this).val())
+             display.val(prevVal);
+        }
+        operator = $(this).val();
+        prevOperator = operator;
+        noSecondArg = 1;
+        console.log(prevOperator);
+        check = 1;
+        status1 = 1;
     });
 }
 
 equal.click( () => {
 
-    if(operator === '+'){
-        prevVal = parseFloat(prevVal) + parseFloat(display.val());
-        console.log(prevVal);
+    if(multiEqual === 1){
+    console.log('prevVal: ' + prevVal);
+        if(prevOperator === '+'){
+            prevVal = parseFloat(prevVal) + parseFloat(prevA);
+        }
+        else if(prevOperator === '-')
+            prevVal =parseFloat(prevVal) - parseFloat(prevA);
+
+        else if(prevOperator === '*')
+            prevVal = parseFloat(prevVal)*parseFloat(prevA);
+
+        else if(prevOperator === '/'){
+            if(display.val() === 0)
+                display.val('infinity');
+            else
+                prevVal = parseFloat(prevVal)/parseFloat(prevA);
+        }
+
+        display.val(prevVal);
     }
-    else if(operator === '-')
-        prevVal =parseFloat(prevVal) - parseFloat(display.val());
 
-    else if(operator === '*')
-        prevVal = parseFloat(prevVal)*parseFloat(display.val());
+    if(status1 !== 0){
 
-    else if(operator === '/')
-        if(display.val() === 0)
-            display.val('infinity');
-        else
-            prevVal = parseFloat(prevVal)/parseFloat(display.val());
+        if(noSecondArg ===0){
 
-    display.val(prevVal);
-    //check = 1;
-    console.log('Equal');
+            if(operator === '+'){
+                prevVal = parseFloat(prevVal) + parseFloat(display.val());
+            }
+            else if(operator === '-')
+                prevVal =parseFloat(prevVal) - parseFloat(display.val());
+
+            else if(operator === '*')
+                prevVal = parseFloat(prevVal)*parseFloat(display.val());
+
+            else if(operator === '/'){
+                if(display.val() === 0)
+                    display.val('infinity');
+                else
+                    prevVal = parseFloat(prevVal)/parseFloat(display.val());
+            }
+
+            display.val(prevVal);
+            check = 1;
+            status1 = 0;
+            multiEqual = 1;
+        }
+
+        else{
+            prevVal = display.val();
+        }
+    }
+
+
 });
 
+
 clear.click( function (){
-    display.val("")
-})
+    display.val("");
+    check = 0;
+    prevVal = 0;
+    prevA  = 0;
+    prevOperator = '';
+    status1 = 0;
+    noSecondArg = 0;
+    multiEqual =0;
+});
+
